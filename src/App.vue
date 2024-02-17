1d3fb198bf7e562338d71210035ab61f7fb49588
bonvi83 <bonvi83@gmail.com>
2024-02-17 11:47:34 +0100

    add function

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,118 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import axios from 'axios';
+  import { store } from './store/';
+
+  import AppHeader from './components/AppHeader.vue';
+
+  export default {
+    data() {
+      return {
+        store,
+        searchedTerm: 'scrubs'
+      }
+    },
+
+    methods: {
+      fetchMovies() {
+        axios.get(this.store.api.uri + '/search/movie',
+          {
+            params: {
+              api_key: store.api.key,
+              query: this.searchedTerm
+            }
+          }
+        ).then((response) => {
+          //uso map() per ottimizzare il mio array dei risultati
+          store.movies = response.data.results.map( (movie) => {
+            return {
+              title: movie.title,
+              original_title: movie.original_title,
+              language: movie.original_language,
+              vote: movie.vote_average
+            }
+          })
+        })
+      },
+
+      fetchTVSeries() {
+        axios.get(this.store.api.uri + '/search/tv',
+          {
+            params: {
+              api_key: store.api.key,
+              query: this.searchedTerm
+            }
+          }
+        ).then((response) => {
+          store.TVseries = response.data.results.map( (TVserie) => {
+            return {
+              name: TVserie.name,
+              original_title: TVserie.original_name,
+              language: TVserie.original_language,
+              vote: TVserie.vote_average
+            }
+          })
+        })
+      },
+
+
+      startResearch() {
+        this.fetchMovies();
+        this.fetchTVSeries();
+      },
+
+      getFlag(lang) {
+        if (lang == 'it') {
+          //gestisco dinamicamente il path relativo dell'immagine
+          return new URL('./assets/img/ita.png', import.meta.url).href;
+        }
+        if (lang == 'en') return new URL('./assets/img/en.png', import.meta.url).href;
+      }
+    },
+
+    components: {AppHeader}
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <h1> {{ this.store.title }} </h1>
+    <input type="text" id="search" v-model="searchedTerm" @keyup.enter="startResearch()">
+    <button type="button" id="submit" @click="startResearch()">CERCA</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <h2>MOVIES</h2>
+
+  <div class="container">
+    <ul v-for="movie in store.movies">
+      <li>Titolo: {{ movie.title }}</li>
+      <li>Titolo originale: {{ movie.original_title }}</li>
+      <li><img :src="getFlag(movie.language)" alt=""></li>
+      <li>Voto: {{ movie.vote }}</li> 
+    </ul>
+  </div>
+
+  <h2>TV SERIES</h2>
+
+  <div class="container">
+    <ul v-for="TVserie in store.TVseries">
+      <li>Titolo: {{ TVserie.name }}</li>
+      <li>Titolo originale: {{ TVserie.original_title }}</li>
+      <li><img :src="getFlag(TVserie.language)" alt=""></li>
+      <li>Voto: {{ TVserie.vote }}</li> 
+    </ul>
+  </div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.container {
+  margin: 0 auto;
+  margin-bottom: 3rem;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+  
+  export default {
+    data() {
+      return {
+      }
+    }
+  };
+</script>
+
+
+
+<style scoped>
+
+
+
+</style>
